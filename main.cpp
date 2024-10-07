f095c5f86c7ed5b9fb560dd9eddd9e4939aad6d3
Nathan Andersen <149338099+NathanAndersen1@users.noreply.github.com>
2024-10-07 14:37:33 +0000

    picking project and setting up classes

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
-r
+#include <iostream>
+
+// class levels
+
+// 1
+#include "StandardTime.hpp"
+// 2
+#include "StandardTrack.hpp"
+#include "StandardCrossCountry.hpp"
+
+
+int main(){
+    // exit condition
+    bool running = true;
+    // the main loop
+    while (running) {
+        std::cout << "test";
+
+    }
+
+    return 0;
+}
+
